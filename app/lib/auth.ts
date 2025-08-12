b49b6759f16e6fbef6bc667e68985e12d891ed1b
Shirooo098 <johndavid8803@gmail.com>
2025-08-12 22:30:57 +0800

    Initalize middleware

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,21 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle"; // your drizzle instance
+import { nextCookies } from "better-auth/next-js";
  
 export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        google: {
+            prompt: "select_account",
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+    plugins: [nextCookies()],
 });
