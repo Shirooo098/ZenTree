12ff06ae27e388df9c475ad56f45ffb1fdb05b36
Shirooo098 <johndavid8803@gmail.com>
2025-08-15 03:39:56 +0800

    Add Routing in logout

--- app/ui/logout.tsx
+++ app/ui/logout.tsx
@@ -1,11 +1,18 @@
 'use client';
+import { useRouter } from "next/navigation";
 import { authClient } from "../lib/auth-client"
 import Button from "./button";
 
 export default function Logout(){
-
+    const router = useRouter();
     const handleLogout = async () => {
-        await authClient.signOut();
+        await authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    router.push('/sign-in')
+                }
+            }
+        });
     }
 
     return(
