10303dc7ae1e242845239e0121b1eed3b9ba17b8
Shirooo098 <johndavid8803@gmail.com>
2025-08-15 03:29:33 +0800

    FIX URL PLS WORK

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { usernameClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins: [usernameClient()] 
 
 })
