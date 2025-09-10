9de8bbc65b764c7c4db45d5e5dc857f81ab7aa55
unknown <marven14panoringan@gmail.com>
2025-09-10 21:42:53 +0800

    adding line under header for design purposes in profile page

--- app/(main)/profile/(overview)/page.tsx
+++ app/(main)/profile/(overview)/page.tsx
@@ -17,7 +17,7 @@ export default async function Profile() {
   return (
     <>
       <div className="w-full flex flex-col ">
-        <div className="flex justify-between items-center w-full px-3 font-sans">
+        <div className="flex justify-between items-center w-full px-3 font-sans border-b border-black pb-5">
           <h1 className="text-2xl font-bold">Profile</h1>
           <h1 className="text-2xl font-bold">Edit Profile</h1>
         </div>
