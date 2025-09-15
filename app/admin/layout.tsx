a2e5217ba79dfab9829e4b46bb872c1eb46be917
Shirooo098 <johndavid8803@gmail.com>
2025-09-15 22:56:59 +0800

    Working sidebar

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -36,7 +36,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
                 }} />
             <SidebarInset>
               <SiteHeader />
-                <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+              <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
             </SidebarInset>
         </SidebarProvider>
     </div>
