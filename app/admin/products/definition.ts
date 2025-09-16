1e0f3a9935972a8d39f2841638fa771eaf480d79
Shirooo098 <johndavid8803@gmail.com>
2025-09-16 14:51:26 +0800

    Initialized Product Props

---
+++ app/admin/products/definition.ts
@@ -0,0 +1,8 @@
+type productProps = {
+    product_id: number,
+    category_id: number | null,
+    product_name: string,
+    product_price: number, 
+    product_description: string,
+    stock: number,
+}
