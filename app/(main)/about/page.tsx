91d41b682ca9480bc6fe458f353f2ab52ad8c3f7
kulastutas <kulaspogi305@gmail.com>
2025-09-19 18:31:57 +0800

    Add about page .tsx

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -2,7 +2,8 @@ import AboutUsHero from "@/app/ui/about/AboutUsHero";
 
 export default function About(){
     return(
-        <><AboutUsHero />
-        </>
+        <div className="">
+            <AboutUsHero />
+        </div>
     )
 }
