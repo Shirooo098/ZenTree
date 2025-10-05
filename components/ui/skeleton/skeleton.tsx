6dffe3b327291188fd69912862f0347f26ee8a8a
unknown <marven14panoringan@gmail.com>
2025-10-05 14:30:00 +0800

    creating checkout page

--- components/ui/skeleton/skeleton.tsx
+++ components/ui/skeleton/skeleton.tsx
@@ -9,7 +9,7 @@ export function CardSkeleton() {
     <div
       className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
     >
-      <div className="flex p-4">
+      <div className="flex p-8">
         <div className="h-5 w-5 rounded-md bg-gray-200" />
         <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
       </div>
