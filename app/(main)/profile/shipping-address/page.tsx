a31e34d873777cfa3cad51713e4fff0e8866cf10
unknown <marven14panoringan@gmail.com>
2025-10-06 13:22:00 +0800

    Shipping Address (feat)

--- app/(main)/profile/shipping-address/page.tsx
+++ app/(main)/profile/shipping-address/page.tsx
@@ -1,14 +1,14 @@
 import { headers } from "next/headers";
 import { auth } from "@/app/lib/auth";
 import { getUserAddresses } from "@/app/actions/address/get-address.action";
-import SideProfile from "@/app/ui/profile/SideProfile";
 import ShippingAddress from "@/app/ui/profile/ShippingAddress";
 
+
 export default async function ShippingAddressPage() {
-  // 1️⃣ Get the logged-in session
+
   const session = await auth.api.getSession({ headers: await headers() });
 
-  // 2️⃣ Handle no user (not logged in)
+
   if (!session || !session.user?.id) {
     return (
       <div className="flex flex-col items-center justify-center h-[400px] text-center">
@@ -19,16 +19,16 @@ export default async function ShippingAddressPage() {
     );
   }
 
-  // 3️⃣ Get userId from session
+
   const userId = session.user.id;
 
-  // 4️⃣ Fetch user’s addresses
+
   const addresses = await getUserAddresses(userId);
 
-  // 5️⃣ Pass userId and addresses into the client component
+
   return (
     <div className="flex gap-10 p-8">
-      <SideProfile />
+    
       <div className="flex-1">
         <ShippingAddress userId={userId} initialAddresses={addresses} />
       </div>
--- app/ui/profile/SideProfile.tsx
+++ app/ui/profile/SideProfile.tsx
@@ -13,7 +13,7 @@ import Logout from '../logout';
 export default function SideProfile() {
   const [activeSection, setActiveSection] = useState("profile");
   return (
-    <div className="flex items-center justify-center gap-7 pl-6 ">
+    <div className="flex justify-center gap-7 pl-6 ">
       <div className="flex flex-col justify-between h-[500px] w-[300px] bg-gray-100 p-10 rounded-md inset-shadow-nav">
        <div className="space-y-4 ">
          <Link href="/profile"
@@ -32,6 +32,12 @@ export default function SideProfile() {
          className="text-left text-lg font-dmSans flex items-center gap-3" >
           <FaIdCard />
           Billing Details
+        </Link>
+
+        <Link href='/profile/shipping-address'
+         className="text-left text-lg font-dmSans flex items-center gap-3" >
+          <FaIdCard />
+          Shipping Address
         </Link>
 
        </div>
