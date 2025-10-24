3d81f41d4f15d93540733cf122878c19a2fd7e1e
4xven <marven14panoringan@gmail.com>
2025-10-24 19:22:08 +0800

    Prevent Staff access to /admin/users

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,17 +1,19 @@
 import { AddUserDialog } from "@/app/ui/admin/dialog/user-dialog/AddUserDialog";
 import UsersTable from "@/app/ui/admin/users/all-users";
-import { getAllUsers } from "@/server/users";
+import { getAllUsers, getCurrentUser } from "@/server/users";
+import { redirect } from "next/navigation";
 
-export default async function Page(){
+export default async function Page() {
+  const user = await getCurrentUser();
+  const allUsers = await getAllUsers();
 
-    const allUsers = await getAllUsers();
-
-    return(
-        <div>
-            <div className="flex justify-end pb-4">
-                <AddUserDialog />
-            </div>
-            <UsersTable usersData={allUsers ?? []}/>
-        </div>
-    )
+  if (user.role !== "admin") redirect("/admin");
+  return (
+    <div>
+      <div className="flex justify-end pb-4">
+        <AddUserDialog />
+      </div>
+      <UsersTable usersData={allUsers ?? []} />
+    </div>
+  );
 }
